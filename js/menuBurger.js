7a1d66c86070b7572f4c53bd8935d963b9df5780
Sergey <GreyTob@inbox.ru>
2021-05-08 20:36:29 +0500

    menu link active

--- js/menuBurger.js
+++ js/menuBurger.js
@@ -9,4 +9,16 @@ document.addEventListener('DOMContentLoaded', function () {
     //при открытом меню не скролится контент страницы
     document.body.classList.toggle('lock')
   })
+
+  //при клике на элемент меню, меню задвигается
+  const li = document.querySelectorAll('.menu a')
+  for (let elem of li) {
+    elem.addEventListener('click', () => {
+      burger.classList.toggle('active')
+      menu.classList.toggle('active')
+    })
+
+    // elem.classList.toggle('active')
+    // console.log(li)
+  }
 })
