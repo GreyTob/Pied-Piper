629c836539e3190f3128f48a9e11812cef561883
Sergey <GreyTob@inbox.ru>
2021-05-08 14:01:03 +0500

    update JS scroll

--- js/headerscroll.js
+++ js/headerscroll.js
@@ -1,10 +1,77 @@
-//плавная пеермотка по якорной ссылке
+//свое решение
+function navHandler() {
+  //выбираю нужный элементы
+  for (let elem of document.querySelectorAll('.header a')) {
+    //вешаю на выбранные элементы события
+    elem.addEventListener('click', function (event) {
+      event.preventDefault()
+
+      //получаю значение атрибута href (this = elem)
+      let id = this.getAttribute('href')
+
+      //нахожу элемент с нужным href
+      const currentElement = document.querySelector(id)
+
+      //получаю координаты нужного элемента
+      function getCoords(elem) {
+        let box = elem.getBoundingClientRect()
+        return {
+          top: box.top + pageYOffset,
+          left: box.left + pageXOffset,
+        }
+      }
+
+      //получаю координату Top
+      const TopCoord = getCoords(currentElement)['top']
+
+      //подумать как закончить скрипт без jQuery
+
+      //перемещаюсь к нужной координате за вычетом величины header - 84px (header {display: fixed})
+      $('body,html').animate({ scrollTop: TopCoord - 84 }, 500)
+    })
+  }
+}
+
+//решения от сюда https://realadmin.ru/coding/js-scroll.html
+//положение block: "start", "center", "end" или "nearest" -  слабо регулируется, а с фиксированым header вообще лажа
+// function navHandler() {
+//   const anchors = document.querySelectorAll('.header a')
+
+//   for (let anchor of anchors) {
+//     anchor.addEventListener('click', function (e) {
+//       e.preventDefault()
+
+//       const blockID = anchor.getAttribute('href')
+
+//       document.querySelector(blockID).scrollIntoView({
+//         behavior: 'smooth',
+//         block: 'center',
+//       })
+//     })
+//   }
+// }
+
+//на jQuery
 //шапка не накрывает контент при переходе по якорным ссылкам
-$(document).ready(function () {
-  $('#menu').on('click', 'a', function (event) {
-    event.preventDefault()
-    let id = $(this).attr('href'),
-      top = $(id).offset().top
-    $('body,html').animate({ scrollTop: top - 84 }, 500)
-  })
-})
+// $(document).ready(function () {
+//   $('#menu').on('click', 'a', function (event) {
+//     event.preventDefault()
+//     let id = $(this).attr('href'),
+//       top = $(id).offset().top
+//     $('body,html').animate({ scrollTop: top - 84 }, 500)
+//   })
+// })
+
+//еще вариан на jQuery; 'a[href^="#"' для всех ссылок с #
+// function navHandler() {
+//   $('a[href^="#"').on('click', function () {
+//     let href = $(this).attr('href')
+
+//     $('html, body').animate({
+//       scrollTop: $(href).offset().top - 84,
+//     })
+//     return false
+//   })
+// }
+
+document.addEventListener('DOMContentLoaded', navHandler)
